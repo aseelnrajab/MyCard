fa76d8253394467b1580b70aa9153e483012130f
aseelnrajab <aseelnrajab1@gmail.com>
2024-11-21 16:52:27 +0200

    Initial Commit

---
+++ MyCard/MyCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCardApp.swift
+//  MyCard
+//
+//  Created by Eng.Aseel on 21/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MyCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
